45a25e40c8647c2b29e995cf56df60be753e678c
Amogh Mannekote <msamogh@gmail.com>
2022-04-20 07:05:00 -0400

    Update files

--- us_city.py
+++ us_city.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 
-def load_us_cities():
-    return np.array(
+def load_us_cities(normalize=True):
+    cities = np.array(
         list(
             map(
                 lambda line: list(map(int, line.split())),
-                open("att48_xy.txt").readlines(),
+                # open("att48_xy.txt").readlines(),
+                open("ena/instances/tsp_22", "r").readlines()
             )
         )
     )
+    norm_cities = (cities - cities.min(axis=0)) / (cities.max(axis=0) - cities.min(axis=0))
+    return cities, norm_cities
